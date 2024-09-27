f6f6773a70c0df11030904fc201e982678a8ecac
ittou-hiramoto <ittou-hiramoto@office-navi.co.jp>
2024-09-27 11:36:57 +0900

    moveCompletedIssuesToCompleteSheetをglobalに追加

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
+import { moveCompletedIssuesToCompleteSheet } from "./moveCompletedIssuesToCompleteSheet";
 import { doPost, onOpen } from "./triggers";
 
 // GAS側から参照したい関数
 (global as any).doPost = doPost;
 (global as any).onOpen = onOpen;
+
+(global as any).moveCompletedIssuesToCompleteSheet =
+  moveCompletedIssuesToCompleteSheet;
